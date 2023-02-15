1602a84e0dcbde9a9e8bbd1f008985d1e17816dc
mizu68 <wanglubao@foundersc.com>
2023-02-15 10:53:25 +0800

    0.2

--- formula_calculator/__calculator.py
+++ formula_calculator/__calculator.py
@@ -4,46 +4,45 @@ from formula_calculator import __operators
 from .__operators import *
 
 
-def calculator(channel,formula,name=None):
+def calculator(channel,**formulas):
     '''
     :param channel: 可迭代对象，迭代返回item数据，item数据格式dict、Series均可
-    :param formula: 公式字符串，传入后可根据公式进行流式数据的计算
-    :param name: 指定公式计算结果的别名，默认为None，若指定了，会将计算结果更新到channel的行情结果中一并返回，key即为name的值
+    :param formulas: 公式集，传入后可对每一个公式进行流式数据的计算。公式的输入应为key-value形式,其中key为计算结果的名称，value为公式内容，且均为字符串，如factor1='max(rolling(high,20))'，或{'factor1':'max(rolling(high,20))'}
     :return: Iterable，
-            每次返回的内容包含channel每次迭代返回的item和公式计算结果，
-            若未指定name，则返回为(item,result),
-            若指定了name，则返回为item，其中item[name]为公式计算结果。
+            每次返回的内容包含channel每次迭代返回的item和公式计算结果，其中item[key]为公式计算结果。
     '''
-
+    buffer_max_size = 1
     if not isinstance(channel,Iterable):
         raise AssertionError("channel参数应为可迭代对象")
-    if not isinstance(formula,str):
-        raise AssertionError("formula参数应为字符串")
-    if name is not None and not isinstance(name,str):
-        raise AssertionError("指定的name参数应为字符串")
-
+    if not isinstance(formulas,dict):
+        raise AssertionError("公式的输入应为key-value形式,其中key为计算结果的名称，value为公式内容，且均为字符串，如factor1='max(rolling(high,20))'，或{'factor1':'max(rolling(high,20))'}")
+    param_list = []
+    formula_list=[]
+    for key,value in formulas.items():
+        if not isinstance(key, str) or not isinstance(value, str):
+            raise AssertionError("公式名和公式应为字符串形式")
+        param_list.append(key)
+        formula_list.append(value)
+        buffer_max_size = max([int(x[1:]) for x in re.findall(r",[0-9.]+", value)] + [buffer_max_size, ])
+    formula = ", ".join(formula_list)
     formula = formula.replace("if(", "ifelse(")
-    nums = re.findall(r"\d+",formula)
-    nums = [int(x) for x in nums]
-    buffer_max_size = max(nums)
-    names = re.findall(r"[a-zA-Z]+",formula)
-
+    variable_list = re.findall(r"[a-zA-Z]+",formula)
     value_list = []
-    for key in set(names):
+    for key in set(variable_list):
         if key not in dir(__operators):
             value_list.append(key)
         elif isinstance(getattr(__operators, key), BufferOpt.__class__):
-            for i in range(names.count(key)):
+            for i in range(variable_list.count(key)):
                 new_name = key+'{0}('.format(i)
                 formula = formula.replace(key+'(',new_name,1)
                 exec("{0}={1}(buffer_max_size)".format(new_name[:-1],key))
+    exec_code = ""
+    for name in param_list:
+        exec_code = exec_code + "item['" + name + "'],"
+    exec_code = exec_code[:-1] + " = " + formula
 
     for item in channel:
         for value in value_list:
             exec("{0}=float({1})".format(value,item[value]))
-        result = eval(formula)
-        if name is None:
-            yield item,result
-        else:
-            item[name] = result
-            yield item
+        exec(exec_code)
+        yield item
