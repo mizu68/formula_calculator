a4109167b5934d13b24b733d69280a5c9810c763
mizu68 <wanglubao@foundersc.com>
2023-02-15 11:09:38 +0800

    0.2

--- formula_calculator/__calculator.py
+++ formula_calculator/__calculator.py
@@ -4,8 +4,11 @@ from formula_calculator import __operators
 from .__operators import *
 
 
-def calculator(channel,**formulas):
+def calculator(channel, **formulas):
     '''
+    ###公式计算器###
+    该函数实现了在channel外的一层装饰器，除了可以迭代获取channel返回的数据外，同时实现formulas中定义的公式计算，并将计算结果update到channel的返回结果中
+
     :param channel: 可迭代对象，迭代返回item数据，item数据格式dict、Series均可
     :param formulas: 公式集，传入后可对每一个公式进行流式数据的计算。公式的输入应为key-value形式,其中key为计算结果的名称，value为公式内容，且均为字符串，如factor1='max(rolling(high,20))'，或{'factor1':'max(rolling(high,20))'}
     :return: Iterable，
@@ -17,16 +20,16 @@ def calculator(channel,**formulas):
     if not isinstance(formulas,dict):
         raise AssertionError("公式的输入应为key-value形式,其中key为计算结果的名称，value为公式内容，且均为字符串，如factor1='max(rolling(high,20))'，或{'factor1':'max(rolling(high,20))'}")
     param_list = []
-    formula_list=[]
+    formula_list = []
     for key,value in formulas.items():
         if not isinstance(key, str) or not isinstance(value, str):
             raise AssertionError("公式名和公式应为字符串形式")
         param_list.append(key)
         formula_list.append(value)
         buffer_max_size = max([int(x[1:]) for x in re.findall(r",[0-9.]+", value)] + [buffer_max_size, ])
-    formula = ", ".join(formula_list)
-    formula = formula.replace("if(", "ifelse(")
-    variable_list = re.findall(r"[a-zA-Z]+",formula)
+    formula_code = ", ".join(formula_list)
+    formula_code = formula_code.replace("if(", "ifelse(")
+    variable_list = re.findall(r"[a-zA-Z]+", formula_code)
     value_list = []
     for key in set(variable_list):
         if key not in dir(__operators):
@@ -34,13 +37,13 @@ def calculator(channel,**formulas):
         elif isinstance(getattr(__operators, key), BufferOpt.__class__):
             for i in range(variable_list.count(key)):
                 new_name = key+'{0}('.format(i)
-                formula = formula.replace(key+'(',new_name,1)
+                formula_code = formula_code.replace(key + '(', new_name, 1)
                 exec("{0}={1}(buffer_max_size)".format(new_name[:-1],key))
+
     exec_code = ""
     for name in param_list:
         exec_code = exec_code + "item['" + name + "'],"
-    exec_code = exec_code[:-1] + " = " + formula
-
+    exec_code = exec_code[:-1] + " = " + formula_code
     for item in channel:
         for value in value_list:
             exec("{0}=float({1})".format(value,item[value]))
